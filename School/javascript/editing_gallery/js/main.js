326b86c89936d6bcf944eac376a5da3f04da5f16
jyee0012 <jyee12@studentmail.nait.ca>
2018-02-01 13:38:57 -0700

    javascript-progress1

--- School/javascript/editing_gallery/js/main.js
+++ School/javascript/editing_gallery/js/main.js
@@ -1,1 +1,13 @@
 // Enter your JavaScript for the solution here...
+
+document.querySelectorAll('.thumbnails img').forEach(function(element) {
+    element.addEventListener('click', function(evt){
+        evt.preventDefault();
+        var image = document.querySelector('.editor img');
+        var newSrc = element.src.split("_"); //find the src tag of the image and replace the _thumbs with ""
+        //console.log(newSrc);
+        //image.src = newSrc[0];
+        image.alt = element.alt;
+        image.title = element.title;
+    });
+}, this);
