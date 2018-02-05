69ab83fdd835b5a301724ed3fc7cc5fdee1d4ba8
jyee0012 <jyee12@studentmail.nait.ca>
2018-02-05 14:49:57 -0700

    javascript-class

---
+++ School/javascript/Exercises/tagging/js/main.js
@@ -0,0 +1,15 @@
+// select the thing
+document.querySelector('.login-form').addEventListener('submit', function(evt){
+    console.log(evt.target.elements);
+    var username = evt.target.elements.username.value;
+    var password = evt.target.elements.password.value;
+    var valid;
+if (valid){
+    evt.preventDefault();
+}
+});
+// create a function to enter the form
+
+// make an eventListener
+
+// do task
