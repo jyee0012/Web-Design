67c7390b8edcb8c783a68ffa400cfe55d8d09c73
jyee0012 <jyee12@studentmail.nait.ca>
2018-02-09 14:27:29 -0700

    inclass exercise

--- School/javascript/Exercises/image-tags/js/main.js
+++ School/javascript/Exercises/image-tags/js/main.js
@@ -1,6 +1,20 @@
-// When the 'Add Tag' submit button is clicked, the p.feature.tags element should be 
-// updated with the tag that was entered by the user.  Do not overwrite any existing tags,
-// simply add each additional tag onto the already existing list.  A tag cannot be empty 
-// (i.e. cannot be whitespace). If the user tries to add an empty tag, remove the hidden 
-// class from the p.feature.error element.  If the form is submitted correctly, then ensure
-// that the p.feature.error element is hidden.
+// When the 'Add Tag' submit button is clicked, the p.feature.tags element should be updated with the tag that was entered by the user. 
+// Do not overwrite any existing tags, simply add each additional tag onto the already existing list. 
+// A tag cannot be empty (i.e. cannot be whitespace). If the user tries to add an empty tag, remove the hidden class from the p.feature.error element. 
+// If the form is submitted correctly, then ensure that the p.feature.error element is hidden.
+document.querySelector('.feature.frm').addEventListener('submit', function(evt){
+    evt.preventDefault();
+    var tags = evt.target.elements.tags.value;
+    var errorMsg = document.querySelector('.feature.error');
+    var displayTag = document.querySelector('.tags');
+    if (tags.trim() == '') {
+        errorMsg.innerHTML = 'Please enter an actual tag.';
+        errorMsg.classList.remove('hidden');
+    } else if (tags.includes(' ')) {
+        errorMsg.innerHTML = 'Your tags cannot contain spaces.';
+        errorMsg.classList.remove('hidden');
+    } else {
+        errorMsg.classList.add('hidden');
+        displayTag.innerHTML = displayTag.innerHTML +'#'+tags+' ';
+    }
+});
