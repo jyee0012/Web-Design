dce44ae6cf0246389711c6caa4352956f04bae7c
jyee0012 <jyee12@studentmail.nait.ca>
2018-02-12 13:40:24 -0700

    done-assignment1-javascript

--- School/javascript/editing_gallery/js/main.js
+++ School/javascript/editing_gallery/js/main.js
@@ -1,8 +1,8 @@
 // Enter your JavaScript for the solution here...
 // double check JavaScript Style Guide.
 
-document.querySelectorAll('.thumbnails img').forEach(function(element) {
-    element.addEventListener('click', function(evt){
+document.querySelectorAll('.thumbnails img').forEach(function (element) {
+    element.addEventListener('click', function(evt) {
         evt.preventDefault();
         var image = document.querySelector('.editor img');
         var newSrc = element.src //find the src tag of the image and replace the _thumbs with ""
@@ -15,28 +15,25 @@ document.querySelectorAll('.thumbnails img').forEach(function(element) {
     });
 }, this);
 
-document.querySelector('form').addEventListener('submit', function(evt){
+document.querySelector('form').addEventListener('submit', function (evt) {
     evt.preventDefault();
     var input = evt.target.elements.tag.value;
     var error = document.querySelector('.error');
-    if (input.trim() == ''){
+    if (input.trim() == '') {
         error.innerHTML = 'Please enter a proper tag';
         error.classList.remove('hidden');
-    }
-    else{
+    } else if (input.includes(' ')) {
+        error.innerHTML = 'Please enter a tag that does not contain spaces';
+        error.classList.remove('hidden');
+    } else {
         error.classList.add('hidden');
         var dataTag;
         var image = document.querySelector('.editor img');
-        document.querySelectorAll('.thumbnails img').forEach(function(element){
-        if (element.alt == image.alt)
-        {
+        document.querySelectorAll('.thumbnails img').forEach(function (element){
+        if (element.alt == image.alt) {
             dataTag = element;
         }
         });
-        for(var i = 0; i < 100; i++)
-        {
-            input = input.replace(' ', '-');
-        }
         var dataTags = dataTag.getAttribute('data-tags'); 
         dataTag.setAttribute('data-tags', dataTags +' '+ input);
         showTags(dataTag);
@@ -44,15 +41,21 @@ document.querySelector('form').addEventListener('submit', function(evt){
     }
 });
 
-function showTags(element){
+function showTags (element) {
     var tagArray = element.getAttribute('data-tags').split(' ');
     var tagString = '';
-    tagArray.forEach(function(tag){
-        if (tag != ''){
+    tagArray.forEach(function(tag) {
+        if (tag != '') {
         tagString = tagString + '#' + tag + ' ';
         }
     });
     document.querySelector('.tags').innerHTML = tagString;
+}
+function replaceSpaces (input) {
+    for(var i = 0; i < 100; i++) {
+        input = input.replace(' ', '-');
+    }
+    return input;
 }
 // find a way to retrieve the submit button and attach an event on it.
 //document.querySelector('input').getAttribute("type");
