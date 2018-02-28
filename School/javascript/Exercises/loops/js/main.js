f9c6d3d200e3fb7948cd3b67c98ba1242d58ca9d
jyee0012 <jyee12@studentmail.nait.ca>
2018-02-28 14:54:27 -0700

    looping

--- School/javascript/Exercises/loops/js/main.js
+++ School/javascript/Exercises/loops/js/main.js
@@ -1,17 +1,32 @@
 function updateOutput () {
     var output = document.querySelector('.output');
-    var list = '<ol>';
+    var list = '<ul>';
+    var num = 10;
     // user input on how many list items there are.
-    list = list + '</ol>';
+    for(var count = 0; count < num; count++){
+        list = list + '<li>' + 'Item ' + (count + 1) + '</li>';
+    }
+    list = list + '</ul>';
     output.innerHTML = list;
 }
-function userInput() {
-    if (confirm('Add a list item?')){
-        do{
-            list = list + '<li>'
-            var input = prompt('What to add?', '');
-            list = list + input + '</li>'
-        }while(confirm('Add another list item?'))
+// updateOutput();
+
+function colorOddRed (){
+    var list = document.querySelectorAll('li');
+    for(var count = 0; count < list.length;count += 2){
+        list[count].classList.add('stripe');
     }
 }
-updateOutput();
+// colorOddRed();
+
+function todoLists(){
+    var todoList = document.querySelector('.todo-list');
+    var list = '<ul>';
+    var num = prompt('How many items?', '');
+    for(var count = 0; count < num;count++){
+        list += '<li><input type="checkbox"><label contenteditable>Item ' + (count + 1) + '</label></li>';
+    }
+    list += '</ul>';
+    todoList.innerHTML = list;
+}
+todoLists();
