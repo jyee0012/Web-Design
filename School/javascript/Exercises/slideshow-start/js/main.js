7c80b249302a2473a9a38b06bde9de1c9b8d197c
jyee0012 <jyee12@studentmail.nait.ca>
2018-03-07 14:57:43 -0700

    slideshow-javascript

---
+++ School/javascript/Exercises/slideshow-start/js/main.js
@@ -0,0 +1,49 @@
+// your js here...
+var images = ['mountain1.jpg', 'mountain2.jpg', 'mountain3.jpg'];
+var currentImg = 0;
+var idx;
+
+// display the current image
+document.querySelector('.carousel>img').src = 'images/' + images[0]; 
+
+// add the appropriate number of selector bullets
+var imgTrackerDIV = document.querySelector('.image-tracker');
+for(idx = 0; idx < images.length;idx++) {
+    imgTrackerDIV.innerHTML += '<span class="control" data-idx="' + (idx) + '">&#8226</span> '
+}
+
+// highlight the first bullet as 'active'
+var periodArray = document.querySelectorAll('.image-tracker span');
+periodArray[currentImg].classList.add('active');
+
+// add event listener for carousel controls
+document.querySelector('.carousel').addEventListener('click', function (evt){
+    var target = evt.target;
+    if (target.classList.contains('control')) {
+        periodArray[currentImg].classList.remove('active');
+        if (target.classList.contains('next')) {
+            // move to the next index in the array
+            currentImg += 1;
+        } else if (target.classList.contains('prev')){
+            // move to the previous index in the array
+            currentImg -= 1;
+        } else {
+        	// selector bullet clicked
+            currentImg = target.dataset.idx;
+        }
+        // if index is outside the array.
+        if (currentImg >= images.length){
+            currentImg = 0;
+        } else if (currentImg < 0){
+            currentImg = images.length - 1;
+        }
+
+        // display the new current image
+        document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
+
+        // update the active selector bullet
+        periodArray[currentImg].classList.add('active');
+        console.log(currentImg + ':' + target.dataset.idx);
+        
+    }
+}); 
--- School/javascript/Exercises/slideshow/js/main.js
+++ School/javascript/Exercises/slideshow/js/main.js
@@ -6,6 +6,7 @@ document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
 document.querySelector('.carousel').addEventListener('click', function (evt){
     var target = evt.target;
     if (target.classList.contains('control')){
+        periodArray[currentImg].classList.remove('active');
         if (target.classList.contains('next')){
             currentImg++;
         } else if (target.classList.contains('prev')){
@@ -17,5 +18,25 @@ document.querySelector('.carousel').addEventListener('click', function (evt){
             currentImg = images.length - 1;
         }
         document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
+        periodArray[currentImg].classList.add('active');
+    }
+});
+
+var imgTrackerDIV = document.querySelector('.image-tracker');
+for(var i = 0; i < images.length;i++) {
+    imgTrackerDIV.innerHTML += '<span>&#8226</span> '
+}
+var periodArray = document.querySelectorAll('.image-tracker > span');
+periodArray[currentImg].classList.add('active');
+imgTrackerDIV.addEventListener('click',function(evt){
+    for(var i = 0; i < periodArray.length;i++) {
+        periodArray[i].addEventListener('click', function(evt){
+            if(!periodArray[i].classList.contains('active')) {
+                periodArray[currentImg].classList.remove('active');
+                currentImg = i;
+                document.querySelector('.carousel>img').src = 'images/' + images[currentImg];
+                periodArray[currentImg].classList.add('active');
+            }
+        });
     }
 });
