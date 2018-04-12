9bc075a118caf78851e5b68e43c8fdd6039bb91f
jyee0012 <jyee12@studentmail.nait.ca>
2018-04-11 21:12:29 -0600

    javascript-im lost

--- School/javascript/friend-finder/js/main.js
+++ School/javascript/friend-finder/js/main.js
@@ -1,1 +1,17 @@
 // Your code here...
+// var friends, friendDetails = [];
+// var xhr = new XMLHttpRequest();
+// xhr.addEventListener('load', function (evt){
+//     friends = JSON.parse(xhr.responseText);
+// });
+// xhr.open('GET','friends.json',true);
+// xhr.send();
+
+// for(var i = 0; i < friends.length; i ++){
+//     var friendXHR = new XMLHttpRequest();
+//     element.addEventListener('load', function (evt){
+//         friendDetails[i] = JSON.parse(friendXHR.responseText);
+//     });
+//     friendXHR.open('GET',friends[i].id + '.json', true);
+//     friendXHR.send();
+// }
