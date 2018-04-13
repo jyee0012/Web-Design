0f1fe17d3c2cb7bc575068f87a8a30ec7102028c
jyee0012 <jyee12@studentmail.nait.ca>
2018-04-13 09:32:55 -0600

    javascript with brandon

--- School/javascript/friend-finder/js/main.js
+++ School/javascript/friend-finder/js/main.js
@@ -1,5 +1,5 @@
 // Your code here...
-var friends, friendDetails = [];
+var friends, friendDetails;
 var xhr = new XMLHttpRequest();
 xhr.addEventListener('load', function (evt) {
     friends = JSON.parse(xhr.responseText);
@@ -9,6 +9,10 @@ xhr.send();
 
 document.querySelector('.pure-menu-link').addEventListener('click', function (evt) {
     console.log(evt.target);
+    friendDetails = JSON.parse(xhr.responseText);
+    xhr.open('GET', 'friends/' + evt.target.data.id + '.json', true);
+    xhr.send();
+    evt.target.data
 });
 // xhr.addEventListener('load', function (evt) {
 //     friendDetails[0] = JSON.parse(xhr.responseText);
@@ -50,14 +54,6 @@ function friendListing() {
     container = document.createElement('div');
     heading = document.createElement('span');
     list = document.createElement('ul');
-    listitems[0] = document.createElement('li');
-    listitems[1] = document.createElement('li');
-    listitems[2] = document.createElement('li');
-    listitems[3] = document.createElement('li');
-    links[0] = document.createElement('a');
-    links[1] = document.createElement('a');
-    links[2] = document.createElement('a');
-    links[3] = document.createElement('a');
 
     //set attributes
     container.setAttribute('class', 'pure-menu custom-restricted-width');
@@ -65,6 +61,8 @@ function friendListing() {
     heading.textContent = 'Friends';
     list.setAttribute('class', 'pure-menu-list');
     for (var i = 0; i < friends.length; i++) {
+        listitems[i] = document.createElement('li');
+        links[i] = document.createElement('a');
         links[i].setAttribute('href', '#');
         links[i].setAttribute('class', 'pure-menu-link');
         links[i].setAttribute('data-id', friends[i].id);
@@ -107,11 +105,11 @@ function showFriendDetails(friendId) {
     homeSpan.textContent = 'hometown:';
     email.appendChild(emailSpan);
     home.appendChild(homeSpan);
-    email.textContent = ' ' + friendDetails[friendId - 1].firstName.toLocaleLowerCase() + friendDetails[friendId - 1].lastName[0].toLocaleLowerCase() + '@example.com';
-    home.textContent = ' edmonton, ab';
+    email.textContent = ' ' + friendDetails[friendId - 1].email;
+    home.textContent = ' ' + friendDetails[friendId -1].home;
 
     bio.setAttribute('class', 'bio');
-    bio.textContent = lorem;
+    bio.textContent = friendDetails[friendId-1].bio;
     // build elements
     list.appendChild(email);
     list.appendChild(home);
